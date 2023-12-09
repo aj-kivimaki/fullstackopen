85632a37dd22c3bc3bff6011a96626c986422670
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-09 15:10:38 +0200

    1.6

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={() => handleClick(text)}>{text}</button>
+);
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = (text) => {
+    if (text === "good") return setGood(good + 1);
+    if (text === "neutral") return setNeutral(neutral + 1);
+    return setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={handleClick} text="good" />
+      <Button handleClick={handleClick} text="neutral" />
+      <Button handleClick={handleClick} text="bad" />
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+};
+
+export default App;
