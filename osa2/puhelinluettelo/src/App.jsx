9fab57f0915cd2182e6515f4fa8c3eb6e364a45a
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-09 23:27:57 +0200

    2.8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1231244" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const personObject = {
       name: newName,
+      number: newNumber,
     };
 
     persons.some((person) => person.name === newName)
@@ -16,6 +20,7 @@ const App = () => {
       : setPersons(persons.concat(personObject));
 
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -27,12 +32,21 @@ const App = () => {
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
