ac4003f4436e69a3f26c4707f14ed0167f69219d
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-09 15:32:59 +0200

    1.8

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -4,20 +4,34 @@ const Button = ({ handleClick, text }) => (
   <button onClick={() => handleClick(text)}>{text}</button>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = good / all;
+
+  return (
+    <>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average || 0}</p>
+      <p>positive {positive || 0} %</p>
+    </>
+  );
+};
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad;
-  const average = (good - bad) / all;
-  const positive = good / all;
-
   const handleClick = (text) => {
-    if (text === "good") return setGood(good + 1);
-    if (text === "neutral") return setNeutral(neutral + 1);
-    return setBad(bad + 1);
+    if (text === "good") return setGood((prev) => prev + 1);
+    if (text === "neutral") return setNeutral((prev) => prev + 1);
+    return setBad((prev) => prev + 1);
   };
 
   return (
@@ -26,13 +40,7 @@ const App = () => {
       <Button handleClick={handleClick} text="good" />
       <Button handleClick={handleClick} text="neutral" />
       <Button handleClick={handleClick} text="bad" />
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average || 0}</p>
-      <p>positive {positive || 0}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
