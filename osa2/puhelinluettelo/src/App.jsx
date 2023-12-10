9cd3e49bc6866d8330105416220b1770459d6873
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-10 20:37:35 +0200

    2.12

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -26,10 +26,13 @@ const App = () => {
 
     persons.some((person) => person.name === newName)
       ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat(personObject));
-
-    setNewName("");
-    setNewNumber("");
+      : axios
+          .post("http://localhost:3001/persons", personObject)
+          .then((response) => {
+            setPersons(persons.concat(response.data));
+            setNewName("");
+            setNewNumber("");
+          });
   };
 
   return (
