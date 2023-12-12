7bc6bef9090aff84afa830f946123760bc8c9c56
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-12 22:26:15 +0200

    2.19

--- osa2/maiden-tiedot/src/App.jsx
+++ osa2/maiden-tiedot/src/App.jsx
@@ -5,6 +5,7 @@ import CountryList from "./components/CountryList";
 function App() {
   const [search, setSearch] = useState("");
   const [countries, setCountries] = useState([]);
+  const [filtered, setFiltered] = useState([]);
 
   useEffect(() => {
     fetch("https://studies.cs.helsinki.fi/restcountries/api/all")
@@ -14,20 +15,27 @@ function App() {
 
   const handleChange = (e) => setSearch(e.target.value);
 
-  const filtered = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(search.toLowerCase())
-  );
+  useEffect(() => {
+    setFiltered(
+      countries.filter((country) =>
+        country.name.common.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search]);
+
+  const handleClick = (country) => setFiltered(country);
 
   return (
     <div className="app">
       <label htmlFor="country">find countries</label>
       <input type="text" id="country" name="coutry" onChange={handleChange} />
       {filtered.length < 10 ? (
-        <CountryList filtered={filtered} />
+        <CountryList filtered={filtered} handleClick={handleClick} />
       ) : (
         <div>Too many matches, specify another filter</div>
       )}
       {filtered.length === 1 && <Country filtered={filtered[0]} />}
+      {filtered.length === 0 && <div>No matches</div>}
     </div>
   );
 }
--- osa2/maiden-tiedot/src/components/CountryList.jsx
+++ osa2/maiden-tiedot/src/components/CountryList.jsx
@@ -1,11 +1,12 @@
-const CountryList = ({ filtered }) => {
-  return (
-    <div>
-      {filtered.length !== 1 &&
-        filtered.map((country) => (
-          <div key={country.name.common}>{country.name.common}</div>
-        ))}
+const CountryList = ({ filtered, handleClick }) => {
+  const countryElement = filtered.map((country) => (
+    <div className="list" key={country.name.common}>
+      <div className="country-name">{country.name.common}</div>
+      <button onClick={() => handleClick([country])}>show</button>
     </div>
-  );
+  ));
+
+  return <div>{filtered.length !== 1 && countryElement}</div>;
 };
+
 export default CountryList;
